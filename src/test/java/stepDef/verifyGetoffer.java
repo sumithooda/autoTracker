5ce13f4b601c69fdd2eecd9553dc81d20816220e
sumit <hoodas172@gmail.com>
2020-03-22 14:23:19 +0100

    update for verify offer class

---
+++ src/test/java/stepDef/verifyGetoffer.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import cucumber.api.java.en.Given;
+
+public class verifyGetoffer {
+    @Given("^customer is on select vehicle screen$")
+    public void customerIsOnSelectVehicleScreen() {
+    }
+}
